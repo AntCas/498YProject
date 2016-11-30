67ebc5641d371a00bbbf4534dbd4184a9708d91e
Stephen Lee <stephenl7797@gmail.com>
2016-11-29 23:01:45 -0500

    changes

--- run_three.sh
+++ run_three.sh
@@ -1,9 +1,9 @@
 #NS_LOG='PartOne:level_info' 498YProject/498YProject 
 
 mkdir three
-mkdir three_hacked
+#mkdir three_hacked
 rm three/*
-rm three_hacked/*
+#rm three_hacked/*
 ../../build/scratch/498YProject/498YProject
 
 python2 make_graph_3.py
