07c3bb11bd2c33dfdf6abe9fa55d5fe56a8eee8d
Anthony Castrio <anthonycastrio@Anthonys-MacBook-Air.local>
2016-12-02 17:42:32 -0500

    added modify_ns3_files.sh to replace hacked ns3 files with modified versions

---
+++ modify_ns3_files.sh
@@ -0,0 +1,9 @@
+# Overwrite the default ns3 files
+cp Hacked_Files/regular-wifi-mac.cc ../../src/wifi/model/regular-wifi-mac.cc
+cp Hacked_Files/spectrum-wifi-phy.cc ../../src/wifi/model/spectrum-wifi-phy.cc
+cp Hacked_Files/wifi-mac.cc ../../src/wifi/model/wifi-mac.cc
+cp Hacked_Files/wifi-mac.h ../../src/wifi/model/wifi-mac.h
+cp Hacked_Files/wifi-phy-standard.h ../../src/wifi/model/wifi-phy-standard.h
+cp Hacked_Files/wifi-phy.cc ../../src/wifi/model/wifi-phy.cc
+cp Hacked_Files/wifi-phy.h ../../src/wifi/model/wifi-phy.h
+
