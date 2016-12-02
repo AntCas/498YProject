84c83035f31e0fed4ea2a04a18c2e1011bee720b
Anthony Castrio <anthonycastrio@Anthonys-MacBook-Air.local>
2016-12-02 15:15:02 -0500

    restored question 2 files

---
+++ run_two.sh
@@ -0,0 +1,11 @@
+#NS_LOG='PartOne:level_info' 498YProject/498YProject 
+
+mkdir rtscts
+mkdir no_rtscts
+rm rtscts/*
+rm no_rtscts/*
+../../build/scratch/498YProject/498YProject
+echo "running question 2"
+
+python2 make_graph.py
+
