ce3b2ba893caf44242bea2b668c47f6d9f29e95c
Anthony Castrio <anthonycastrio@gmail.com>
2016-12-04 16:27:02 -0500

    udpate run files

--- one_/run_one.sh
+++ one_/run_one.sh
@@ -1,14 +1,4 @@
 #NS_LOG='PartOne:level_info' 498YProject/498YProject 
-
-mkdir a_rtscts
-mkdir a_no_rtscts
-mkdir b_rtscts
-mkdir b_no_rtscts
-rm a_rtscts/*
-rm a_no_rtscts/*
-rm b_rtscts/*
-rm b_no_rtscts/*
-
-../../build/scratch/one/one
-python2 make_graph.py
+../../build/scratch/one_/one_
+python make_graph.py
 
--- three_/run_three.sh
+++ three_/run_three.sh
@@ -1,11 +1,4 @@
 #NS_LOG='PartOne:level_info' 498YProject/498YProject 
-
-mkdir three_data
-#mkdir three_hacked
-rm three_data/*
-#rm three_hacked/*
-../../build/scratch/three/three
-echo "running question 2"
-
-python2 make_graph_3.py
+../../build/scratch/three_/three_
+python make_graph_3.py
 
