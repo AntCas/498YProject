d5ef497560e52ebdb8006c4584a9e36c4401c866
Anthony Castrio <anthonycastrio@gmail.com>
2016-12-04 15:34:40 -0500

    update relative path of modified ns3 files

--- modify_ns3_files.sh
+++ modify_ns3_files.sh
@@ -1,9 +1,9 @@
 # Overwrite the default ns3 files
-cp .hacked_files/regular-wifi-mac.cc ../src/wifi/model/regular-wifi-mac.cc
-cp .hacked_files/spectrum-wifi-phy.cc ../src/wifi/model/spectrum-wifi-phy.cc
-cp .hacked_files/wifi-mac.cc ../src/wifi/model/wifi-mac.cc
-cp .hacked_files/wifi-mac.h ../src/wifi/model/wifi-mac.h
-cp .hacked_files/wifi-phy-standard.h ../src/wifi/model/wifi-phy-standard.h
-cp .hacked_files/wifi-phy.cc ../src/wifi/model/wifi-phy.cc
-cp .hacked_files/wifi-phy.h ../src/wifi/model/wifi-phy.h
+cp .hacked_files/regular-wifi-mac.cc ../../src/wifi/model/regular-wifi-mac.cc
+cp .hacked_files/spectrum-wifi-phy.cc ../../src/wifi/model/spectrum-wifi-phy.cc
+cp .hacked_files/wifi-mac.cc ../../src/wifi/model/wifi-mac.cc
+cp .hacked_files/wifi-mac.h ../../src/wifi/model/wifi-mac.h
+cp .hacked_files/wifi-phy-standard.h ../../src/wifi/model/wifi-phy-standard.h
+cp .hacked_files/wifi-phy.cc ../../src/wifi/model/wifi-phy.cc
+cp .hacked_files/wifi-phy.h ../../src/wifi/model/wifi-phy.h
 
